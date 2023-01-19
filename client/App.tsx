03f613fc776110e051382c15d1a30360debf178b
hongdahee <heewhy3513@gmail.com>
2023-01-20 02:39:00 +0900

    [CHORE]: 기초 세팅

---
+++ client/App.tsx
@@ -0,0 +1,22 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={{ color: "white" }}>
+        Open up App.tsx to start working on your app!
+      </Text>
+      <StatusBar style="light" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#22222",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
