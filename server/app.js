c49e5563f1c99fbfbb841d9fc1984a80a1c2dd69
Exist95 <jyeol0210@naver.com>
2023-01-22 14:58:49 +0900

    [CHORE]: vercel 에러 핸들링

--- server/app.js
+++ server/app.js
@@ -9,9 +9,17 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: path.join(__dirname, "public") });
+});
+
 app.use("/api/users", usersRoute);
 
 app.use("/api/question", questionRoute);
+
+module.exports = app;
 
 dotenv.config();
 
