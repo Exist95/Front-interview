aeced0fe3a70b91eadd9f7acd99b71163f5bfb13
hongdahee <heewhy3513@gmail.com>
2023-01-24 01:25:33 +0900

    [STYLE]: 문제 페이지 ui 구현

--- client/view/Questions/index.tsx
+++ client/view/Questions/index.tsx
@@ -1,29 +1,24 @@
-import { Text } from "react-native";
+import { Text, TouchableOpacity } from "react-native";
 import * as S from "./style";
-
+const answers = ["파이썬", "자바스크립트", "자바", "타입스크립트"];
 export const QuestionsTemp = () => {
   return (
     <S.Container>
       <S.QContainer>
-        <S.Title>Q.</S.Title>
+        <S.Title>Q1.</S.Title>
         <S.QContent>질문입니다.</S.QContent>
       </S.QContainer>
       <S.AContainer>
         <S.Title>A.</S.Title>
         <S.ScrollView>
           <S.BtnContainer>
-            <S.Answer>
-              <Text>1번</Text>
-            </S.Answer>
-            <S.Answer>
-              <Text>1번</Text>
-            </S.Answer>
-            <S.Answer>
-              <Text>1번</Text>
-            </S.Answer>
-            <S.Answer>
-              <Text>1번</Text>
-            </S.Answer>
+            {answers.map((answer) => {
+              return (
+                <S.Answer>
+                  <S.AContent>{answer}</S.AContent>
+                </S.Answer>
+              );
+            })}
           </S.BtnContainer>
         </S.ScrollView>
       </S.AContainer>
