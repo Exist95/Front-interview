7a46aa10f1b984d60ff1ac388b28f1fed89c8d29
Exist95 <jyeol0210@naver.com>
2023-01-24 17:18:52 +0900

    [FEAT]: 결과 페이지 구현 시작

---
+++ client/view/Result/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Header } from "../components/common/Header";
+import * as S from "./style";
+
+export const ResultTemp = () => {
+  return (
+    <S.Container>
+      <Header />
+    </S.Container>
+  );
+};
