f19097a99059ff661fa31c79e4f2e7539cead810
Exist95 <jyeol0210@naver.com>
2023-01-24 19:35:32 +0900

    [STYLE]: 결과 페이지 스타일 수정

--- client/view/Result/style.ts
+++ client/view/Result/style.ts
@@ -7,3 +7,12 @@ export const Container = styled.View`
   justify-content: space-between;
   padding: 20% 5%;
 `;
+
+export const UserResult = styled.Text`
+  color: white;
+  font-weight: 600;
+  font-size: 40px;
+  align-items: center;
+  text-align: center;
+  flex: 1.5;
+`;
