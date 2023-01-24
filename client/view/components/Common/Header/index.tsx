6b7a7fa203930649393469022fa33502b81635e7
Exist95 <jyeol0210@naver.com>
2023-01-24 17:19:22 +0900

    [FEAT]: 헤더 로고 추가

--- client/view/components/Common/Header/index.tsx
+++ client/view/components/Common/Header/index.tsx
@@ -1,12 +1,14 @@
 import { useNavigation } from "@react-navigation/native";
-import { Pressable, Text } from "react-native";
+import { Image, Pressable, Text } from "react-native";
 import * as S from "./style";
 
 export const Header = () => {
   const navigation = useNavigation<any>();
   return (
     <S.Container>
-      <Text>logo</Text>
+      <S.Logo
+        source={require("../../../../assets/fterview_icon-removebg-preview.png")}
+      />
       <Pressable
         onPress={() => {
           navigation.navigate("MyPage");
--- client/view/components/Common/Header/style.ts
+++ client/view/components/Common/Header/style.ts
@@ -1,17 +1,20 @@
 import styled from "styled-components/native";
 
-const Container = styled.View`
+export const Container = styled.View`
   flex: 1;
   flex-direction: row;
   justify-content: space-between;
   width: 100%;
 `;
 
-const Profile = styled.View`
-  width: 60;
-  height: 60;
-  border-radius: 30;
+export const Profile = styled.View`
+  width: 60px;
+  height: 60px;
+  border-radius: 30px;
   background-color: gray;
 `;
 
-export {Container, Profile};
+export const Logo = styled.Image`
+  width: 60px;
+  height: 60px;
+`;
