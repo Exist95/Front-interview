319e142e1b57677f540ffa84900a5340a1803edc
Exist95 <jyeol0210@naver.com>
2023-01-24 19:34:24 +0900

    [FEAT]: 다크모드 버튼

---
+++ client/view/MyPage/DarkMode/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import * as S from "./style";
+
+export const DarkMode = () => {
+  return (
+    <S.DarkMode
+      source={require("../../../assets/fterview_icon-removebg-preview.png")}
+    />
+  );
+};
