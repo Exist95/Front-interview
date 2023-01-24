4fce5ef21b98bb57482de901f4cd13788fcc02a7
Exist95 <jyeol0210@naver.com>
2023-01-24 17:19:55 +0900

    [STYLE]: 사이즈 단위 추가

--- client/view/Main/style.ts
+++ client/view/Main/style.ts
@@ -1,6 +1,6 @@
 import styled from "styled-components/native";
 
-const Container = styled.View`
+export const Container = styled.View`
   flex: 1;
   background-color: #232323;
   align-items: center;
@@ -8,11 +8,9 @@ const Container = styled.View`
   padding: 20% 5%;
 `;
 
-const MainTitle = styled.Text`
+export const MainTitle = styled.Text`
   flex: 2;
   color: white;
   font-weight: 600;
-  font-size: 70%;
+  font-size: 100px;
 `;
-
-export { Container, MainTitle };
---
+++ client/view/MyPage/style.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: #232323;
+  align-items: center;
+  justify-content: space-between;
+  padding: 20% 5%;
+`;
