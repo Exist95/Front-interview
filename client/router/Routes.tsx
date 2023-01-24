b8a5991e0b9306b5cffb69d32a26c4a1a466b954
Exist95 <jyeol0210@naver.com>
2023-01-24 17:18:30 +0900

    [FEAT]: Tab에 Result 페이지 임시 적용

--- client/router/Routes.tsx
+++ client/router/Routes.tsx
@@ -2,6 +2,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Main } from "../pages/Main";
 import { MyPage } from "../pages/MyPage";
 import { Questions } from "../pages/Questions";
+import Result from "../pages/Result";
 
 const Stack = createNativeStackNavigator();
 
@@ -15,6 +16,7 @@ export const StackNavigator = () => {
         <Stack.Screen name="Main" component={Main} />
         <Stack.Screen name="Questions" component={Questions} />
         <Stack.Screen name="MyPage" component={MyPage} />
+        <Stack.Screen name="Result" component={Result} />
       </Stack.Navigator>
     </>
   );
