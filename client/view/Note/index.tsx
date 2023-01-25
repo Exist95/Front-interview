b84615299d4c1f7350e93141e254b3a2b7b12a05
Exist95 <jyeol0210@naver.com>
2023-01-26 02:49:57 +0900

    [FEAT]: 오답노트 페이지 구현

--- client/view/Note/index.tsx
+++ client/view/Note/index.tsx
@@ -1,5 +1,26 @@
-import { Text } from "react-native";
+import { Header } from "../components/Common/Header";
+import { NoteList } from "./NoteList";
+import * as S from "./style";
+
+const dummy = [
+  {
+    question: "무엇이 문제인가요?",
+    answer: "정답인가요?",
+    part: "HTML",
+  },
+];
 
 export const NoteTemp = () => {
-  return <Text>Note</Text>;
+  return (
+    <S.Container>
+      <Header />
+      <S.TextBox>
+        <S.NoteTitle>오답노트</S.NoteTitle>
+        <S.NotePoint>40/900</S.NotePoint>
+      </S.TextBox>
+      {dummy.map((item: any, i) => {
+        return <NoteList key={i} item={item} />;
+      })}
+    </S.Container>
+  );
 };
--- client/view/Note/style.ts
+++ client/view/Note/style.ts
@@ -1,9 +1,29 @@
 import styled from "styled-components/native";
 
 export const Container = styled.View`
-  flex: 1;
   background-color: ${(props) => props.theme.bgColor};
+  height: 100%;
   align-items: center;
-  justify-content: space-between;
   padding: 20% 5%;
+`;
+
+export const TextBox = styled.View`
+  width: 100%;
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const NoteTitle = styled.Text`
+  font-size: 20px;
+  font-weight: 800;
+  color: ${(props) => props.theme.textColor};
+`;
+
+export const NotePoint = styled.Text`
+  font-size: 20px;
+  font-weight: 800;
+
+  color: ${(props) => props.theme.textColor};
 `;
