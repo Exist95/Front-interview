a7307ff430c27644759338b77214327880c883f4
hongdahee <107875003+hongdahee@users.noreply.github.com>
2023-02-02 15:26:40 +0900

    [FEAT]: 접근 제한 페이지 테스트
    * [CHORE]: vercel 배포 취소
    
    * [CHORE]: 구글 클라우드 배포 성공
    
    * [CHORE]: 안 쓰는 코드 제거
    
    * [STYLE]: 로그인 페이지 ui 구현
    
    * [FEAT]: 회원가입 페이지 구현
    
    * [FEAT]: 데이터 패칭 세팅
    
    * [FEAT]: post 데이터 패칭
    
    * [FEAT]: 로그인, 회원가입 데이터 패칭
    
    * [FEAT]: 회원가입 기능 구현
    
    * [FEAT]: 로그인 여부 상태 추가
    
    * [FEAT]: 로그인 여부에 따른 렌더링
    
    * [FEAT]: 접근 제한 페이지 테스트

--- client/view/Result/index.tsx
+++ client/view/Result/index.tsx
@@ -1,19 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { NavigationButton } from "../components/Common/NavigationButton";
 import { Header } from "../components/Common/Header";
 import * as S from "./style";
 import { useNavigation } from "@react-navigation/native";
 import { LoginViewModel } from "../../vm/LoginViewModel";
 import { Login } from "../../pages/Auth/Login";
+import { Text } from "react-native";
 
 export const ResultTemp = () => {
   const navigation = useNavigation<any>();
   const { isLogin } = LoginViewModel();
 
+  useEffect(() => {
+    if (!isLogin) navigation.navigate("Login");
+  }, []);
+
   return (
-    <>
+    <S.Container>
       {isLogin ? (
-        <S.Container>
+        <>
           <Header />
           <S.UserResult>
             User님의{"\n"}
@@ -21,10 +26,13 @@ export const ResultTemp = () => {
             80점입니다.
           </S.UserResult>
           <NavigationButton text={"홈으로"} destination={"Main"} />
-        </S.Container>
+        </>
       ) : (
-        <Login />
+        <>
+          <Text>접근할 수 없는 페이지입니다. 로그인해 주세요!</Text>
+          <NavigationButton text={"로그인 하러 가기"} destination={"Login"} />
+        </>
       )}
-    </>
+    </S.Container>
   );
 };
