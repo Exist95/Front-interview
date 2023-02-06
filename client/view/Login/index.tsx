b743c25e4bbf633c624238d621d3e475ee693572
Exist95 <jyeol0210@naver.com>
2023-02-06 09:11:17 +0900

    [STYLE]: 스타일 미세 조정

--- client/view/Login/index.tsx
+++ client/view/Login/index.tsx
@@ -53,7 +53,6 @@ export const LoginTemp = () => {
           }}
           onPress={(e: any) => EmailSaveCheck(e)}
         />
-        <S.ChangePassword>비밀번호 변경</S.ChangePassword>
       </S.SubManuBox>
       <NavigationButton text="로그인" destination="Main" />
 
